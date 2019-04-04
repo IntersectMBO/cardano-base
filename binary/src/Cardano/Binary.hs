d56b164eba4be6cba3954fecb0c413657112ffd3
Rupert Horlick <rupert.horlick@iohk.io>
2019-04-04 15:18:37 +0100

    Export Cardano.Binary.Raw

--- binary/src/Cardano/Binary.hs
+++ binary/src/Cardano/Binary.hs
@@ -8,5 +8,6 @@ import Cardano.Binary.Deserialize as X
 import Cardano.Binary.Drop as X
 import Cardano.Binary.FromCBOR as X
 import Cardano.Binary.Limit as X
+import Cardano.Binary.Raw as X
 import Cardano.Binary.Serialize as X
 import Cardano.Binary.ToCBOR as X
