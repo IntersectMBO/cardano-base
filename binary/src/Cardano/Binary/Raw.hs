9dd082842bd8d7742d88c9cee46a144e9b65f02e
Rupert Horlick <rupert.horlick@iohk.io>
2019-04-05 15:36:47 +0100

    Add missing To/FromCBOR instances to Raw

--- binary/src/Cardano/Binary/Raw.hs
+++ binary/src/Cardano/Binary/Raw.hs
@@ -7,9 +7,12 @@ where
 
 import Cardano.Prelude
 
+import Cardano.Binary.FromCBOR (FromCBOR)
+import Cardano.Binary.ToCBOR (ToCBOR)
+
 
 -- | A wrapper over 'ByteString' for signalling that a bytestring should be
 --   processed as a sequence of bytes, not as a separate entity. It's used in
 --   crypto and binary code.
 newtype Raw = Raw ByteString
-  deriving (Eq, Ord, Show, NFData)
+  deriving (Eq, Ord, Show, NFData, FromCBOR, ToCBOR)
