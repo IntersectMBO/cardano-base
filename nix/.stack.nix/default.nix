21278bcf44f54db8eaee4db04fb84af03f1bfeaf
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-07-03 09:16:25 +1000

    Nix voodoo

--- nix/.stack.nix/default.nix
+++ nix/.stack.nix/default.nix
@@ -6,7 +6,6 @@
         "hedgehog" = (((hackage.hedgehog)."1.0").revisions).default;
         "micro-recursion-schemes" = (((hackage.micro-recursion-schemes)."5.0.2.2").revisions).default;
         "streaming-binary" = (((hackage.streaming-binary)."0.3.0.1").revisions).default;
-        "pretty-show" = (((hackage.pretty-show)."1.8.2").revisions).default;
         } // {
         cardano-binary = ./cardano-binary.nix;
         cardano-binary-test = ./cardano-binary-test.nix;
@@ -20,6 +19,6 @@
       compiler.version = "8.6.5";
       compiler.nix-name = "ghc865";
       };
-  resolver = "lts-13.24";
+  resolver = "lts-13.26";
   compiler = "ghc-8.6.5";
   }
