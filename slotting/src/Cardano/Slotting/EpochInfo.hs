e530bbb7e799ab36a743ca0799f7659b5d1261c7
Nicholas Clarke <nick@topos.org.uk>
2019-11-22 12:34:05 +0100

    Move Slot related primitives into cardano-base

---
+++ slotting/src/Cardano/Slotting/EpochInfo.hs
@@ -0,0 +1,8 @@
+module Cardano.Slotting.EpochInfo
+  ( module Cardano.Slotting.EpochInfo.API,
+    module Cardano.Slotting.EpochInfo.Impl,
+  )
+where
+
+import Cardano.Slotting.EpochInfo.API
+import Cardano.Slotting.EpochInfo.Impl
