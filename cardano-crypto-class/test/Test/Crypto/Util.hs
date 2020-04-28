4bebb8fac2fa048a56ca2a2d4f28e9a1626bcd8e
Duncan Coutts <duncan@well-typed.com>
2020-04-28 12:26:00 +0100

    Extend the DSIGN serialisation tests to cover raw serialisation
    Also extend them to cover the direct use of the DSIGN class methods to
    encode/decode, as well as the To/FromCBOR instnaces. Also add tests to
    check that these two CBOR methods are consistent with each other.

--- cardano-crypto-class/test/Test/Crypto/Util.hs
+++ cardano-crypto-class/test/Test/Crypto/Util.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Test.Crypto.Util
   ( -- * CBOR
     FromCBOR (..)
   , ToCBOR (..)
   , prop_cbor
+  , prop_cbor_with
   , prop_cbor_valid
   , prop_cbor_roundtrip
+  , prop_raw_serialise
+  , prop_cbor_direct_vs_class
   , -- * Test Seed
     TestSeed (..)
   , withTestSeed
@@ -18,7 +24,9 @@ module Test.Crypto.Util
   )
 where
 
-import Cardano.Binary (FromCBOR (..), ToCBOR (..), decodeFull, serialize)
+import Cardano.Binary (FromCBOR (..), ToCBOR (..),
+                       Encoding, Decoder,
+                       decodeFullDecoder, serializeEncoding)
 import Codec.CBOR.FlatTerm
 import Crypto.Random
   ( ChaChaDRG
@@ -26,6 +34,7 @@ import Crypto.Random
   , drgNewTest
   , withDRG
   )
+import Data.ByteString (ByteString)
 import Data.Word (Word64)
 import Numeric.Natural (Natural)
 import Test.QuickCheck
@@ -59,25 +68,66 @@ testSeedToChaCha = drgNewTest . getTestSeed
 nullTestSeed :: TestSeed
 nullTestSeed = TestSeed (0, 0, 0, 0, 0)
 
--- Class properties
---
-prop_cbor :: (ToCBOR a, FromCBOR a, Eq a, Show a) => a -> Property
-prop_cbor x =
-  prop_cbor_valid x .&&.
-    prop_cbor_roundtrip x
 
-prop_cbor_valid :: ToCBOR a => a -> Property
-prop_cbor_valid a =
-  let e = toCBOR a
-      f = toFlatTerm e
-      s = "invalid flat term " ++ show f ++ " from encoding " ++ show e
-  in counterexample s $ validFlatTerm f
+--------------------------------------------------------------------------------
+-- Serialisation properties
+--------------------------------------------------------------------------------
+
+prop_cbor :: (ToCBOR a, FromCBOR a, Eq a, Show a)
+          => a -> Property
+prop_cbor = prop_cbor_with toCBOR fromCBOR
+
+
+prop_cbor_with :: (Eq a, Show a)
+               => (a -> Encoding)
+               -> (forall s. Decoder s a)
+               -> a -> Property
+prop_cbor_with encoder decoder x =
+      prop_cbor_valid     encoder         x
+ .&&. prop_cbor_roundtrip encoder decoder x
+
+
+prop_cbor_valid :: (a -> Encoding) -> a -> Property
+prop_cbor_valid encoder x =
+    counterexample errmsg $
+      validFlatTerm term
+  where
+    term     = toFlatTerm encoding
+    encoding = encoder x
+    errmsg   = "invalid flat term " ++ show term
+            ++ " from encoding " ++ show encoding
+
 
 -- Written like this so that an Eq DeserialiseFailure is not required.
-prop_cbor_roundtrip :: (ToCBOR a, FromCBOR a, Eq a, Show a) => a -> Property
-prop_cbor_roundtrip x = case decodeFull (serialize x) of
-  Right y -> y === x
-  Left decoderError -> counterexample (show decoderError) (property False)
+prop_cbor_roundtrip :: (Eq a, Show a)
+                    => (a -> Encoding)
+                    -> (forall s. Decoder s a)
+                    -> a -> Property
+prop_cbor_roundtrip encoder decoder x =
+    case decodeFullDecoder "" decoder (serializeEncoding (encoder x)) of
+      Right y  -> y === x
+      Left err -> counterexample (show err) (property False)
+
+
+prop_raw_serialise :: (Eq a, Show a)
+                   => (a -> ByteString)
+                   -> (ByteString -> Maybe a)
+                   -> a -> Property
+prop_raw_serialise serialise deserialise x =
+    case deserialise (serialise x) of
+      Just y  -> y === x
+      Nothing -> property False
+
+-- | The crypto algorithm classes have direct encoding functions, and the key
+-- types are also typically a member of the 'ToCBOR' class. Where a 'ToCBOR'
+-- instance is provided then these should match.
+--
+prop_cbor_direct_vs_class :: ToCBOR a
+                          => (a -> Encoding)
+                          -> a -> Property
+prop_cbor_direct_vs_class encoder x =
+  toFlatTerm (encoder x) === toFlatTerm (toCBOR x)
+
 
 --------------------------------------------------------------------------------
 -- Natural numbers
