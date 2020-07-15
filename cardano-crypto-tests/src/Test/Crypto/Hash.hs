6fdc88b53759def3450eb3e4ca3541d9f68817a0
Tobias Dammers <tdammers@gmail.com>
2020-07-15 12:33:08 +0200

    Regression tests for SHA256, MD5

--- cardano-crypto-tests/src/Test/Crypto/Hash.hs
+++ cardano-crypto-tests/src/Test/Crypto/Hash.hs
@@ -17,6 +17,14 @@ import Test.Crypto.Util (prop_cbor, prop_cbor_size, prop_no_unexpected_thunks)
 import Test.QuickCheck
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
+import Test.Tasty.HUnit (testCase, assertEqual, assertFailure, assertBool, Assertion)
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.ByteArray.Encoding (convertFromBase, convertToBase, Base (Base16))
+import Data.Char (ord)
+import Data.Word (Word8)
+import Control.Monad (forM_)
 
 --
 -- The list of all tests
@@ -29,6 +37,8 @@ tests =
     , testHashAlgorithm (Proxy :: Proxy SHA3_256) "SHA3_256"
     , testHashAlgorithm (Proxy :: Proxy Blake2b_224) "Blake2b_224"
     , testHashAlgorithm (Proxy :: Proxy Blake2b_256) "Blake2b_256"
+    , testHashRegressions (Proxy :: Proxy SHA256) "fixtures/sha256.input"
+    , testHashRegressions (Proxy :: Proxy MD5) "fixtures/md5.input"
     ]
 
 testHashAlgorithm
@@ -61,6 +71,42 @@ prop_hash_correct_sizeHash h =
 prop_hash_show_fromString :: Hash h a -> Property
 prop_hash_show_fromString h = h === fromString (show h)
 
+testHashRegressions
+  :: forall h. (HashAlgorithm h)
+  => Proxy h
+  -> FilePath
+  -> TestTree
+testHashRegressions p fp =
+  testCase ("Hash regressions " <> fp) (test_hash_regressions p fp)
+
+test_hash_regressions
+  :: forall h. (HashAlgorithm h)
+  => Proxy h
+  -> FilePath
+  -> Assertion
+test_hash_regressions p fixtureFilePath = do
+  fixture <- parseFixture <$> BS.readFile fixtureFilePath
+  forM_ (zip [1..] fixture) $ \(lineNum :: Int, (m, hm)) -> do
+    let h = digest p m
+    assertEqual
+          ("hash " ++ fixtureFilePath ++ ":" ++ show lineNum)
+          (convertToBase Base16 hm :: ByteString)
+          (convertToBase Base16 h :: ByteString)
+    let forged = BS.pack . forge . BS.unpack $ m
+        hf = digest p forged
+    assertBool ("forged " ++ fixtureFilePath ++ ":" ++ show lineNum) (hf /= hm)
+  where
+    forge :: [Word8] -> [Word8]
+    forge [] = [fromIntegral $ ord 'x']
+    forge xs = init xs ++ [last xs + 1]
+
+    parseFixture input =
+      [ ( either error id (convertFromBase Base16 m) :: ByteString
+        , either error id (convertFromBase Base16 hm) :: ByteString
+        )
+      | [m, hm] <-
+        map (BS.split (fromIntegral $ ord ':')) (BS.split (fromIntegral $ ord '\n') input)
+      ]
 
 --
 -- Arbitrary instances
