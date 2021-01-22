32c124f2e67d834d14ba1da9099d85be17e8e05c
John Ky <john.ky@iohk.io>
2021-01-22 18:59:46 +1100

    Revert "Upgrade to ghc-8.10.3 in nix builds"
    This reverts commit b9fe831d1fc03de5b773542d22bcdbf7addebd1d.

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -7,7 +7,7 @@
 , buildPackages
 , config ? {}
 # GHC attribute name
-, compiler ? config.haskellNix.compiler or "ghc8103"
+, compiler ? config.haskellNix.compiler or "ghc8102"
 # Enable profiling
 , profiling ? config.haskellNix.profiling or false
 }:
