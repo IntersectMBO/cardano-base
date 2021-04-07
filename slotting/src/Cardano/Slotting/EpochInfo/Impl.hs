aae019c862e40a2cf856cdc11c985b2e4af9ee13
Nicolas Frisby <nick.frisby@iohk.io>
2021-04-06 17:20:34 -0700

    Add comments and shortcuts to Cardano.Slotting.EpochInfo.Impl
    These shortcuts are used in eg the Consensus Layer mocks and tests.

--- slotting/src/Cardano/Slotting/EpochInfo/Impl.hs
+++ slotting/src/Cardano/Slotting/EpochInfo/Impl.hs
@@ -1,5 +1,9 @@
+-- | For use in trivial cases, such as in mocks, tests, etc.
 module Cardano.Slotting.EpochInfo.Impl
   ( fixedEpochInfo,
+    -- * Shortcuts
+    fixedEpochInfoEpoch,
+    fixedEpochInfoFirst,
   )
 where
 
@@ -7,14 +11,30 @@ import Cardano.Slotting.EpochInfo.API
 import Cardano.Slotting.Slot (EpochNo (..), EpochSize (..), SlotNo (..))
 import Cardano.Slotting.Time (RelativeTime (..), SlotLength, getSlotLength)
 
+-- | The 'EpochInfo' induced by assuming the epoch size and slot length are
+-- fixed for the entire system lifetime
 fixedEpochInfo :: Monad m => EpochSize -> SlotLength -> EpochInfo m
 fixedEpochInfo (EpochSize size) slotLength = EpochInfo
   { epochInfoSize_ = \_ ->
       return $ EpochSize size,
-    epochInfoFirst_ = \(EpochNo epochNo) ->
-      return $ SlotNo (epochNo * size),
-    epochInfoEpoch_ = \(SlotNo slot) ->
-      return $ EpochNo (slot `div` size),
+    epochInfoFirst_ = \e -> return $ fixedEpochInfoFirst (EpochSize size) e,
+    epochInfoEpoch_ = \sl -> return $ fixedEpochInfoEpoch (EpochSize size) sl,
     epochInfoSlotToRelativeTime_ = \(SlotNo slot) ->
       return $ RelativeTime (fromIntegral slot * getSlotLength slotLength)
   }
+
+-- | The pure computation underlying 'epochInfoFirst' applied to
+-- 'fixedEpochInfo'
+--
+-- You don't need a 'SlotLength' for this.
+fixedEpochInfoFirst :: EpochSize -> EpochNo -> SlotNo
+fixedEpochInfoFirst (EpochSize size) (EpochNo epochNo) =
+  SlotNo (epochNo * size)
+
+-- | The pure computation underlying 'epochInfoEpoch' applied to
+-- 'fixedEpochInfo'
+--
+-- You don't need a 'SlotLength' for this.
+fixedEpochInfoEpoch :: EpochSize -> SlotNo -> EpochNo
+fixedEpochInfoEpoch (EpochSize size) (SlotNo slot) =
+  EpochNo (slot `div` size)
