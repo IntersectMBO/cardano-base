c028519d1180e42d6ab3ddf97d6fa57395966c2e
iquerejeta <31273774+iquerejeta@users.noreply.github.com>
2021-07-22 11:17:39 +0100

    adding Keccak256 to cardano-crypto (#221)

--- cardano-crypto-class/src/Cardano/Crypto/Hash.hs
+++ cardano-crypto-class/src/Cardano/Crypto/Hash.hs
@@ -11,3 +11,4 @@ import Cardano.Crypto.Hash.NeverUsed as X
 import Cardano.Crypto.Hash.SHA256 as X
 import Cardano.Crypto.Hash.SHA3_256 as X
 import Cardano.Crypto.Hash.Short as X
+import Cardano.Crypto.Hash.Keccak256 as X
