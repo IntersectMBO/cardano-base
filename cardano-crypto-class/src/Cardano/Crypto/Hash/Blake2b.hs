26ba8fce352375abeaa282f6c9db2ddbf52d19a3
iquerejeta <querejeta.azurmendi@iohk.io>
2021-07-22 14:52:36 +0100

    libsodium as backend for sha and blake

--- cardano-crypto-class/src/Cardano/Crypto/Hash/Blake2b.hs
+++ cardano-crypto-class/src/Cardano/Crypto/Hash/Blake2b.hs
@@ -10,9 +10,19 @@ module Cardano.Crypto.Hash.Blake2b
   )
 where
 
+import Control.Monad (unless)
+import Cardano.Crypto.Libsodium.C
+
 import Cardano.Crypto.Hash.Class
-import qualified "cryptonite" Crypto.Hash as H
-import qualified Data.ByteArray as BA
+import Foreign.ForeignPtr (withForeignPtr, mallocForeignPtrBytes)
+import Foreign.Ptr (castPtr, nullPtr)
+import Foreign.C.Error (errnoToIOError, getErrno)
+import Data.Proxy
+import GHC.TypeLits (natVal)
+import GHC.IO.Exception (ioException)
+import System.IO.Unsafe (unsafePerformIO)
+
+import qualified Data.ByteString as B
 
 data Blake2b_224
 data Blake2b_256
@@ -20,9 +30,21 @@ data Blake2b_256
 instance HashAlgorithm Blake2b_224 where
   type SizeHash Blake2b_224 = 28
   hashAlgorithmName _ = "blake2b_224"
-  digest _ = BA.convert . H.hash @_ @H.Blake2b_224
+  digest _ = blake2b_libsodium (fromIntegral (natVal (Proxy::Proxy (SizeHash Blake2b_224))))
 
 instance HashAlgorithm Blake2b_256 where
   type SizeHash Blake2b_256 = 32
   hashAlgorithmName _ = "blake2b_256"
-  digest _ = BA.convert . H.hash @_ @H.Blake2b_256
+  digest _ = blake2b_libsodium (fromIntegral (natVal (Proxy::Proxy (SizeHash Blake2b_256))))
+
+blake2b_libsodium :: Int -> ByteString -> ByteString
+blake2b_libsodium size = \input -> unsafePerformIO $ do
+  output <- mallocForeignPtrBytes size
+  withForeignPtr output $ \output' -> do
+    B.useAsCStringLen input $ \(ptr, inputlen) -> do
+      res <- c_crypto_generichash_blake2b output' (fromIntegral size) (castPtr ptr) (fromIntegral inputlen) nullPtr 0 -- we used unkeyed hash
+      unless (res == 0) $ do
+        errno <- getErrno
+        ioException $ errnoToIOError "digest @Blake2b: crypto_generichash_blake2b" errno Nothing Nothing
+
+      B.packCStringLen (output', size)
--- cardano-crypto-class/src/Cardano/Crypto/Hash/SHA256.hs
+++ cardano-crypto-class/src/Cardano/Crypto/Hash/SHA256.hs
@@ -8,16 +8,41 @@ module Cardano.Crypto.Hash.SHA256
   )
 where
 
+import Control.Monad (unless)
+import Cardano.Crypto.Libsodium.C
+import Cardano.Foreign (SizedPtr(SizedPtr))
 import Cardano.Crypto.Hash.Class
-import qualified "cryptonite" Crypto.Hash as H
-import qualified Data.ByteArray as BA
+
+-- Make all imports specific (or think about it)
+import Foreign.ForeignPtr (withForeignPtr, mallocForeignPtrBytes)
+import Foreign.Ptr (castPtr)
+import Foreign.C.Error (errnoToIOError, getErrno)
+import Data.Proxy
+import GHC.TypeLits (natVal)
+import GHC.IO.Exception (ioException)
+import System.IO.Unsafe (unsafePerformIO)
+
+import qualified Data.ByteString as B
+
 
 data SHA256
 
 instance HashAlgorithm SHA256 where
   type SizeHash SHA256 = 32
   hashAlgorithmName _ = "sha256"
-  digest _ = convert . H.hash
+  digest _ = sha256_libsodium
 
-convert :: H.Digest H.SHA256 -> ByteString
-convert = BA.convert
+sha256_libsodium :: ByteString -> ByteString
+sha256_libsodium input = unsafePerformIO $ do
+  output <- mallocForeignPtrBytes expected_size
+  withForeignPtr output $ \output' -> do
+    B.useAsCStringLen input $ \(ptr, inputlen) -> do
+      res <- c_crypto_hash_sha256 (SizedPtr (castPtr output')) (castPtr ptr) (fromIntegral inputlen)
+      unless (res == 0) $ do
+          errno <- getErrno
+          ioException $ errnoToIOError "digest @SHA256: c_crypto_hash_sha256" errno Nothing Nothing
+
+    B.packCStringLen (output', expected_size)
+
+  where
+    expected_size = fromIntegral (natVal (Proxy::Proxy (SizeHash SHA256)))
