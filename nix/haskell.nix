d4bb854e6f68bc7c7b9e29a07890af3bdc67664f
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-03-07 14:41:00 +0000

    Bump iohk-nix and drop nixpkgs pin
    It looks like the nixpkgs pin from `haskell.nix` *is* actually new
    enough.
    
    I didn't get cache hits until I switched to 8.10.7, which seems like a
    good idea anyway.

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -7,7 +7,7 @@
 , buildPackages
 , config ? {}
 # GHC attribute name
-, compiler ? config.haskellNix.compiler or "ghc8105"
+, compiler ? config.haskellNix.compiler or "ghc8107"
 # Enable profiling
 , profiling ? config.haskellNix.profiling or false
 }:
