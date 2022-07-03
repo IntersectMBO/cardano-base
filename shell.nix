ae031e13a3818d8a1cb9fc7fdb3f483a07be6dd2
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2022-07-03 15:47:02 +0300

    Fix issue with cabal unable to verify ssl certificates
    ```
    $ cabal test all
    Cloning into '/home/lehins/iohk/cardano-base/dist-newstyle/src/cardano-p_-98329c466570048a'...
    fatal: unable to access 'https://github.com/input-output-hk/cardano-prelude/': SSL certificate problem: unable to get local issuer certificate
    ```

--- shell.nix
+++ shell.nix
@@ -34,9 +34,6 @@ let
     # *all* dependencies are provided by Nix.
     exactDeps = false;
 
-    NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
-    SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
-
     inherit withHoogle;
   };
 
