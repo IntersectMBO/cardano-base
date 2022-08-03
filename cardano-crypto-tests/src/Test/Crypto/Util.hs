709b9be90ce3409931d6545fa22c57c8e83077d2
Koz Ross <koz.ross@retro-freedom.nz>
2022-08-04 11:14:53 +1200

    Add deserialization tests, pin repro

--- cardano-crypto-tests/src/Test/Crypto/Util.hs
+++ cardano-crypto-tests/src/Test/Crypto/Util.hs
@@ -4,6 +4,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE RoleAnnotations #-}
+{-# LANGUAGE TupleSections #-}
 
 module Test.Crypto.Util
   ( -- * CBOR
@@ -15,6 +19,7 @@ module Test.Crypto.Util
   , prop_cbor_valid
   , prop_cbor_roundtrip
   , prop_raw_serialise
+  , prop_raw_deserialise
   , prop_size_serialise
   , prop_cbor_direct_vs_class
 
@@ -32,14 +37,40 @@ module Test.Crypto.Util
 
    -- * test messages for signings
   , Message(..)
+
+    -- * Test generation and shrinker helpers
+  , BadInputFor
+  , genBadInputFor
+  , shrinkBadInputFor
+  , showBadInputFor
   )
 where
 
-import Cardano.Binary (FromCBOR (..), ToCBOR (..),
-                       Encoding, Decoder, Range (..),
-                       decodeFullDecoder, serializeEncoding, szGreedy, szSimplify)
-import Codec.CBOR.FlatTerm
-import Codec.CBOR.Write
+import Numeric (showHex)
+import GHC.Exts (fromListN, fromList, toList)
+import Text.Show.Pretty (ppShow)
+import Data.Kind (Type)
+import Cardano.Binary (
+  FromCBOR (fromCBOR), 
+  ToCBOR (toCBOR),
+  Encoding, 
+  Decoder, 
+  Range (Range),
+  decodeFullDecoder, 
+  serializeEncoding, 
+  szGreedy, 
+  szSimplify,
+  lo, 
+  hi,
+  encodedSizeExpr
+  )
+import Codec.CBOR.FlatTerm  (
+  validFlatTerm,
+  toFlatTerm
+  )
+import Codec.CBOR.Write (
+  toStrictByteString
+  )
 import Cardano.Crypto.Seed (Seed, mkSeedFromBytes)
 import Cardano.Crypto.Util (SignableRepresentation(..))
 import Crypto.Random
@@ -48,8 +79,9 @@ import Crypto.Random
   , drgNewTest
   , withDRG
   )
-import Data.ByteString as BS (ByteString, pack, unpack, length)
-import Data.Proxy (Proxy (..))
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.Proxy (Proxy (Proxy))
 import Data.Word (Word64)
 import NoThunks.Class (NoThunks, unsafeNoThunks)
 import Numeric.Natural (Natural)
@@ -65,8 +97,12 @@ import Test.QuickCheck
   , property
   , shrink
   , vector
+  , checkCoverage
+  , cover
   )
-import Formatting.Buildable (Buildable (..))
+import Formatting.Buildable (build)
+import qualified Test.QuickCheck.Gen as Gen
+import Control.Monad (guard, when)
 
 --------------------------------------------------------------------------------
 -- Connecting MonadRandom to Gen
@@ -85,7 +121,6 @@ testSeedToChaCha = drgNewTest . getTestSeed
 
 nullTestSeed :: TestSeed
 nullTestSeed = TestSeed (0, 0, 0, 0, 0)
-
 
 instance Arbitrary TestSeed where
   arbitrary =
@@ -113,7 +148,6 @@ instance Arbitrary Message where
   arbitrary = Message . BS.pack <$> arbitrary
   shrink    = map (Message . BS.pack) . shrink . BS.unpack . messageBytes
 
-
 --------------------------------------------------------------------------------
 -- Serialisation properties
 --------------------------------------------------------------------------------
@@ -133,7 +167,6 @@ prop_cbor_size a = counterexample (show lo ++ " ≰ " ++ show len) (lo <= len)
         Right x -> x
         Left err -> error . show . build $ err
 
-
 prop_cbor_with :: (Eq a, Show a)
                => (a -> Encoding)
                -> (forall s. Decoder s a)
@@ -141,7 +174,6 @@ prop_cbor_with :: (Eq a, Show a)
 prop_cbor_with encoder decoder x =
       prop_cbor_valid     encoder         x
  .&&. prop_cbor_roundtrip encoder decoder x
-
 
 prop_cbor_valid :: (a -> Encoding) -> a -> Property
 prop_cbor_valid encoder x =
@@ -153,7 +185,6 @@ prop_cbor_valid encoder x =
     errmsg   = "invalid flat term " ++ show term
             ++ " from encoding " ++ show encoding
 
-
 -- Written like this so that an Eq DeserialiseFailure is not required.
 prop_cbor_roundtrip :: (Eq a, Show a)
                     => (a -> Encoding)
@@ -164,7 +195,6 @@ prop_cbor_roundtrip encoder decoder x =
       Right y  -> y === x
       Left err -> counterexample (show err) (property False)
 
-
 prop_raw_serialise :: (Eq a, Show a)
                    => (a -> ByteString)
                    -> (ByteString -> Maybe a)
@@ -173,6 +203,20 @@ prop_raw_serialise serialise deserialise x =
     case deserialise (serialise x) of
       Just y  -> y === x
       Nothing -> property False
+
+prop_raw_deserialise :: 
+  forall (a :: Type) .
+  (Show a) =>
+  (ByteString -> Maybe a) -> 
+  BadInputFor a -> 
+  Property
+prop_raw_deserialise deserialise (BadInputFor (forbiddenLen, bs)) = 
+  checkCoverage . 
+  cover 50.0 (BS.length bs > forbiddenLen) "too long" .
+  cover 50.0 (BS.length bs < forbiddenLen) "too short" $
+  case deserialise bs of 
+    Nothing -> property True
+    Just x -> counterexample (ppShow x) . property $ False
 
 -- | The crypto algorithm classes have direct encoding functions, and the key
 -- types are also typically a member of the 'ToCBOR' class. Where a 'ToCBOR'
@@ -183,7 +227,6 @@ prop_cbor_direct_vs_class :: ToCBOR a
                           -> a -> Property
 prop_cbor_direct_vs_class encoder x =
   toFlatTerm (encoder x) === toFlatTerm (toCBOR x)
-
 
 prop_size_serialise :: (a -> ByteString) -> Word -> a -> Property
 prop_size_serialise serialise size x =
@@ -198,3 +241,54 @@ prop_no_thunks :: NoThunks a => a -> Property
 prop_no_thunks !a = case unsafeNoThunks a of
     Nothing  -> property True
     Just msg -> counterexample (show msg) (property False)
+
+--------------------------------------------------------------------------------
+-- Helpers for property testing
+--------------------------------------------------------------------------------
+
+-- Essentially a ByteString carrying around the length it's not allowed to be.
+-- This is annoying, but so's QuickCheck sometimes.
+newtype BadInputFor (a :: Type) = BadInputFor (Int, ByteString)
+  deriving (Eq) via (Int, ByteString)
+  deriving stock (Show)
+
+-- Coercion around a phantom parameter here is dangerous, as there's an implicit
+-- relation between it and the forbidden length. We ensure this is impossible.
+type role BadInputFor nominal
+
+-- Needed instead of an Arbitrary instance, as there's no (good) way of knowing
+-- what our forbidden (i.e. correct) length is.
+genBadInputFor :: 
+  forall (a :: Type) . 
+  Int -> 
+  Gen (BadInputFor a)
+genBadInputFor forbiddenLen = 
+  BadInputFor . (forbiddenLen,) <$> Gen.oneof [tooLow, tooHigh]
+  where
+    tooLow :: Gen ByteString
+    tooLow = do
+      len <- Gen.chooseInt (0, forbiddenLen - 1)
+      fromListN len <$> Gen.vectorOf len arbitrary
+    tooHigh :: Gen ByteString
+    tooHigh = do
+      len <- Gen.chooseInt (forbiddenLen + 1, forbiddenLen * 2)
+      fromListN len <$> Gen.vectorOf len arbitrary
+
+-- This ensures we don't \'shrink out of case\': we shrink too-longs to
+-- (smaller) too-longs, and too-shorts to (smaller) too-shorts.
+shrinkBadInputFor :: 
+  forall (a :: Type) . 
+  BadInputFor a -> 
+  [BadInputFor a]
+shrinkBadInputFor (BadInputFor (len, bs)) = BadInputFor . (len,) <$> do
+  bs' <- fromList <$> (shrink . toList $ bs)
+  when (BS.length bs > len) (guard (BS.length bs' > len))
+  pure bs'
+
+-- This shows only the ByteString, in hex.
+showBadInputFor :: 
+  forall (a :: Type) . 
+  BadInputFor a -> 
+  String
+showBadInputFor (BadInputFor (_, bs)) = 
+  "0x" <> BS.foldr showHex "" bs <> " (length " <> (show . BS.length $ bs) <> ")"
