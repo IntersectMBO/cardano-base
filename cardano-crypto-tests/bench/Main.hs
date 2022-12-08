053205c5d15164bddfd47e6969c88d7f7bcbc760
Tobias Dammers <tdammers@gmail.com>
2022-12-08 12:46:44 +0100

    Lock KES operations when benchmarking

--- cardano-crypto-tests/bench/Main.hs
+++ cardano-crypto-tests/bench/Main.hs
@@ -7,14 +7,17 @@ import qualified Bench.Crypto.DSIGN (benchmarks)
 import qualified Bench.Crypto.KES   (benchmarks)
 import qualified Bench.Crypto.VRF   (benchmarks)
 
+import Control.Concurrent.MVar
+
 main :: IO ()
 main = do
   sodiumInit
-  defaultMain benchmarks
+  kesLock <- newMVar ()
+  defaultMain (benchmarks kesLock)
 
-benchmarks :: [Benchmark]
-benchmarks =
+benchmarks :: MVar () -> [Benchmark]
+benchmarks kesLock =
   [ Bench.Crypto.DSIGN.benchmarks
-  , Bench.Crypto.KES.benchmarks
+  , Bench.Crypto.KES.benchmarks kesLock
   , Bench.Crypto.VRF.benchmarks
   ]
--- cardano-crypto-tests/src/Bench/Crypto/KES.hs
+++ cardano-crypto-tests/src/Bench/Crypto/KES.hs
@@ -20,11 +20,13 @@ import Cardano.Crypto.KES.Class
 import Cardano.Crypto.KES.Sum
 import Cardano.Crypto.KES.CompactSum
 
+import Control.Concurrent.MVar
+import Control.Exception (bracket)
+
 import Criterion
 import qualified Data.ByteString as BS (ByteString)
 import Data.Either (fromRight)
 import Cardano.Crypto.Libsodium as NaCl
-import Test.Crypto.RunIO (RunIO (..))
 import System.IO.Unsafe (unsafePerformIO)
 import GHC.TypeLits (KnownNat)
 import Data.Kind (Type)
@@ -37,48 +39,53 @@ import Bench.Crypto.BenchData
 testSeedMLSB :: forall n. KnownNat n => NaCl.MLockedSizedBytes n
 testSeedMLSB = unsafePerformIO $ NaCl.mlsbFromByteString testBytes
 
+withKESLock :: MVar () -> IO a -> IO a
+withKESLock kesLock action =
+  bracket
+    (takeMVar kesLock)
+    (putMVar kesLock)
+    (const action)
 
-benchmarks :: Benchmark
-benchmarks = bgroup "KES"
-  [ benchKES @Proxy @IO @(Sum6KES Ed25519DSIGNM Blake2b_256) Proxy Proxy "Sum6KES"
-  , benchKES @Proxy @IO @(Sum7KES Ed25519DSIGNM Blake2b_256) Proxy Proxy "Sum7KES"
-  , benchKES @Proxy @IO @(CompactSum6KES Ed25519DSIGNM Blake2b_256) Proxy Proxy "CompactSum6KES"
-  , benchKES @Proxy @IO @(CompactSum7KES Ed25519DSIGNM Blake2b_256) Proxy Proxy "CompactSum7KES"
+benchmarks :: MVar () -> Benchmark
+benchmarks kesLock = bgroup "KES"
+  [ benchKES @Proxy @(Sum6KES Ed25519DSIGNM Blake2b_256) Proxy kesLock "Sum6KES"
+  , benchKES @Proxy @(Sum7KES Ed25519DSIGNM Blake2b_256) Proxy kesLock "Sum7KES"
+  , benchKES @Proxy @(CompactSum6KES Ed25519DSIGNM Blake2b_256) Proxy kesLock "CompactSum6KES"
+  , benchKES @Proxy @(CompactSum7KES Ed25519DSIGNM Blake2b_256) Proxy kesLock "CompactSum7KES"
   ]
 
 
 
 {-# NOINLINE benchKES #-}
-benchKES :: forall (proxy :: forall k. k -> Type) m v
-           . ( KESSignAlgorithm m v
+benchKES :: forall (proxy :: forall k. k -> Type) v
+           . ( KESSignAlgorithm IO v
              , ContextKES v ~ ()
              , Signable v BS.ByteString
              , NFData (SignKeyKES v)
              , NFData (SigKES v)
-             , RunIO m
              )
-          => proxy m
-          -> proxy v
+          => proxy v
+          -> MVar ()
           -> [Char]
           -> Benchmark
-benchKES _ _ lbl =
+benchKES _ kesLock lbl =
   bgroup lbl
     [ bench "genKey" $
-        nfIO . io $ genKeyKES @m @v testSeedMLSB >>= forgetSignKeyKES @m @v
+        nfIO . withKESLock kesLock $ genKeyKES @IO @v testSeedMLSB >>= forgetSignKeyKES @IO @v
     , bench "signKES" $
-        nfIO . io $
-          (\sk -> do { sig <- signKES @m @v () 0 typicalMsg sk; forgetSignKeyKES sk; return sig })
-            =<< (genKeyKES @m @v testSeedMLSB)
+        nfIO . withKESLock kesLock $
+          (\sk -> do { sig <- signKES @IO @v () 0 typicalMsg sk; forgetSignKeyKES sk; return sig })
+            =<< (genKeyKES @IO @v testSeedMLSB)
     , bench "verifyKES" $
-        nfIO . io $ do
-          signKey <- genKeyKES @m @v testSeedMLSB
-          sig <- signKES @m @v () 0 typicalMsg signKey
+        nfIO . withKESLock kesLock $ do
+          signKey <- genKeyKES @IO @v testSeedMLSB
+          sig <- signKES @IO @v () 0 typicalMsg signKey
           verKey <- deriveVerKeyKES signKey
           forgetSignKeyKES signKey
           return . fromRight $ verifyKES @v () verKey 0 typicalMsg sig
     , bench "updateKES" $
-        nfIO . io $ do
-          signKey <- genKeyKES @m @v testSeedMLSB
+        nfIO . withKESLock kesLock $ do
+          signKey <- genKeyKES @IO @v testSeedMLSB
           sk' <- fromJust <$> updateKES () signKey 0
           forgetSignKeyKES signKey
           return sk'
