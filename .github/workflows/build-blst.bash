3eaac9c91514a98df39f00e27171b2ca37fe5782
Moritz Angermann <moritz.angermann@gmail.com>
2023-01-11 12:19:24 +0800

    Update build-blst.bash

--- .github/workflows/build-blst.bash
+++ .github/workflows/build-blst.bash
@@ -1,3 +1,7 @@
+#!/bin/bash
+# I don't understand why this just vanishes.
+export PATH=/usr/bin:$PATH
+
 mkdir blst-sources && cd blst-sources
 git clone https://github.com/supranational/blst
 cd blst
