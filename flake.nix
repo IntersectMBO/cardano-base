f3592f6bf1809966436683edbaf917d76d75d26d
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2023-07-26 16:22:10 +0100

    Remove cicero

--- flake.nix
+++ flake.nix
@@ -1,16 +1,12 @@
 {
   inputs = {
     haskellNix.url = "github:input-output-hk/haskell.nix";
-    haskellNix.inputs.tullia.follows = "tullia";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     iohkNix.url = "github:input-output-hk/iohk-nix";
     flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
 
     CHaP.url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
     CHaP.flake = false;
-
-    # cicero
-    tullia.url = "github:input-output-hk/tullia";
 
     # non-flake nix compatibility
     flake-compat.url = "github:edolstra/flake-compat";
@@ -108,61 +104,6 @@
           # we also want cross compilation to windows.
           nixpkgs.lib.optionalAttrs (system == "x86_64-linux") {
           crossPlatforms = p: [p.mingwW64];
-        })
-        # add cicero logic.
-        // (let actionCiInputName = "GitHub event"; in inputs.tullia.fromSimple system {
-          tasks =  {
-            ci = { config, lib, ... }: {
-              preset = {
-                nix.enable = true;
-                github.ci = {
-                  # Tullia tasks can run locally or on Cicero.
-                  # When no facts are present we know that we are running locally and vice versa.
-                  # When running locally, the current directory is already bind-mounted into the container,
-                  # so we don't need to fetch the source from GitHub and we don't want to report a GitHub status.
-                  enable = config.actionRun.facts != {};
-                  repository = "input-output-hk/cardano-base";
-                  remote = config.preset.github.lib.readRepository actionCiInputName null;
-                  revision = config.preset.github.lib.readRevision actionCiInputName null;
-                };
-              };
-
-
-              command.text = config.preset.github.status.lib.reportBulk {
-                bulk.text = ''
-                  nix eval .#hydraJobs --apply __attrNames --json |
-                  nix-systems -i |
-                  jq 'with_entries(select(.value))' # filter out systems that we cannot build for
-                '';
-                each.text = ''nix build -L .#hydraJobs."$1".required'';
-                skippedDescription = lib.escapeShellArg "No nix builder for this system";
-              };
-
-              memory = 1024 * 8;
-              nomad.driver = "exec";
-              nomad.resources.cpu = 10000;
-            };
-          };
-
-          actions = {
-            "cardano-base/ci" = {
-              task = "ci";
-              io = ''
-                // This is a CUE expression that defines what events trigger a new run of this action.
-                // There is no documentation for this yet. Ask SRE if you have trouble changing this.
-                let github = {
-                  #input: "${actionCiInputName}"
-                  #repo: "input-output-hk/cardano-base"
-                }
-
-                #lib.merge
-                #ios: [
-                  {#lib.io.github_push, github, #default_branch: true},
-                  {#lib.io.github_pr,   github},
-                ]
-              '';
-            };
-          };
         });
       in nixpkgs.lib.recursiveUpdate flake {
         # add a required job, that's basically all hydraJobs.
