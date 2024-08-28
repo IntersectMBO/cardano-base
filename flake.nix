2d879a84fbd233220d27543cec0220c93ab33010
Pablo Lamela <pablo.lamela@iohk.io>
2024-08-28 02:16:20 +0200

    Start integrating the JS bit (still failing)

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
 
   outputs = inputs:
     let
+      lib = inputs.nixpkgs.lib;
       profiling = false;
       supportedSystems = [
         "x86_64-linux"
@@ -32,13 +33,16 @@
           inherit system;
           inherit (inputs.haskellNix) config;
         };
+
+        defaultCompilerVersion = "ghc928";
+
         # ... and construct a flake from the cabal.project file.
         # We use cabalProject' to ensure we don't build the plan for
         # all systems.
-        flake = (nixpkgs.haskell-nix.cabalProject' rec {
+        flake = (nixpkgs.haskell-nix.cabalProject' ({config, ...}: {
           src = ./.;
           name = "cardano-base";
-          compiler-nix-name = "ghc928";
+          compiler-nix-name = lib.mkDefault defaultCompilerVersion;
 
           # CHaP input map, so we can find CHaP packages (needs to be more
           # recent than the index-state we set!). Can be updated with
@@ -85,7 +89,7 @@
               packages.slotting.configureFlags = [ "--ghc-option=-Werror" ];
               enableLibraryProfiling = profiling;
             })
-            ({pkgs, ...}: with pkgs; nixpkgs.lib.mkIf stdenv.hostPlatform.isWindows {
+            ({pkgs, ...}: with pkgs; lib.mkIf stdenv.hostPlatform.isWindows {
               packages.text.flags.simdutf = false;
               # Disable cabal-doctest tests by turning off custom setups
               packages.comonad.package.buildType = lib.mkForce "Simple";
@@ -102,13 +106,83 @@
               packages.terminal-size.components.library.build-tools = lib.mkForce [];
               packages.network.components.library.build-tools = lib.mkForce [];
             })
+
+            # GHCJS build configuration
+            ({ config, pkgs, ... }:
+              let
+                # Run the script to build the C sources from cryptonite and cardano-crypto
+                # and place the result in jsbits/cardano-crypto.js
+                jsbits = pkgs.runCommand "cardano-addresses-jsbits" { } ''
+                  script=$(mktemp -d)
+                  cp -r ${cardano-addresses/cardano-addresses-jsbits/emscripten}/* $script
+                  ln -s ${pkgs.srcOnly {name = "cryptonite-src"; src = config.packages.cryptonite.src;}}/cbits $script/cryptonite
+                  ln -s ${pkgs.srcOnly {name = "cardano-crypto-src"; src = config.packages.cardano-crypto.src;}}/cbits $script/cardano-crypto
+                  patchShebangs $script/build.sh
+                  (cd $script && PATH=${
+                      # The extra buildPackages here is for closurecompiler.
+                      # Without it we get `unknown emulation for platform: js-unknown-ghcjs` errors.
+                      lib.makeBinPath (with pkgs.buildPackages.buildPackages;
+                        [emscripten closurecompiler coreutils])
+                    }:$PATH ./build.sh)
+                  mkdir -p $out
+                  cp $script/cardano-crypto.js $out
+                '';
+                addJsbits = ''
+                  mkdir -p jsbits
+                  cp ${jsbits}/* jsbits
+                '';
+              in
+              lib.mkIf (pkgs.stdenv.hostPlatform.isGhcjs) {
+                reinstallableLibGhc = false;
+      
+                # TODO replace this with `zlib` build with `emcc` if possible.
+                # Replace zlib with a derivation including just the header files
+                packages.digest.components.library.libs = lib.mkForce [(
+                  pkgs.pkgsBuildBuild.runCommand "zlib" { nativeBuildInputs = [ pkgs.pkgsBuildBuild.xorg.lndir ]; } ''
+                    mkdir -p $out/include
+                    lndir ${pkgs.pkgsBuildBuild.lib.getDev pkgs.pkgsBuildBuild.zlib}/include $out/include
+                '')];
+                # Prevent downstream packages from looking for zlib
+                packages.digest.components.library.postInstall = ''
+                  sed -i 's/^extra-libraries: *z//g' $out/package.conf.d/digest-*.conf
+                '';
+                # Prevent errors from missing zlib function _adler32
+                packages.cardano-addresses.configureFlags = [ "--gcc-options=-Wno-undefined" ];
+                packages.cardano-addresses-cli.configureFlags = [ "--gcc-options=-Wno-undefined" ];
+                packages.cardano-addresses-jsapi.configureFlags = [ "--gcc-options=-Wno-undefined" ];
+      
+                packages.cardano-addresses-cli.components.library.build-tools = [ pkgs.buildPackages.buildPackages.gitMinimal ];
+                packages.cardano-addresses-jsapi.components.library.build-tools = [ pkgs.buildPackages.buildPackages.gitMinimal ];
+                packages.cardano-addresses-jsbits.components.library.preConfigure = addJsbits;
+                packages.cardano-addresses-cli.components.tests.unit.preCheck = ''
+                  export CARDANO_ADDRESSES_CLI="${config.hsPkgs.cardano-addresses-cli.components.exes.cardano-address}/bin"
+                '';
+                packages.cardano-addresses-cli.components.tests.unit.build-tools = pkgs.lib.mkForce [
+                  config.hsPkgs.buildPackages.hspec-discover.components.exes.hspec-discover
+                  pkgs.buildPackages.nodejs
+               ];
+             })
+            ({ pkgs, ... }: lib.mkIf (!pkgs.stdenv.hostPlatform.isGhcjs) {
+                # Disable jsapi-test on jsaddle/native. It's not working yet.
+                packages.cardano-addresses-jsapi.components.tests.jsapi-test.preCheck = ''
+                  echo "Tests disabled on non-ghcjs"
+                  exit 0
+                '';
+              })
           ];
-        }).flake (
-          # we also want cross compilation to windows.
-          nixpkgs.lib.optionalAttrs (system == "x86_64-linux") {
-          crossPlatforms = p: [p.mingwW64];
-        });
-      in nixpkgs.lib.recursiveUpdate flake {
+          flake = {
+          # on linux, build/test other supported compilers
+          variants = lib.optionalAttrs (system == "x86_64-linux")
+            # on linux, build/test other supported compilers
+            (lib.genAttrs ["ghc810"] (compiler-nix-name: {
+              inherit compiler-nix-name;
+            }));
+          crossPlatforms = p:
+            lib.optional (system == "x86_64-linux" && builtins.elem config.compiler-nix-name ["ghc8107"]) p.ghcjs ++
+            lib.optional (system == "x86_64-linux" && config.compiler-nix-name == defaultCompilerVersion) p.mingwW64;
+          };
+        })).flake {};
+      in lib.recursiveUpdate flake {
         # add a required job, that's basically all hydraJobs.
         hydraJobs = nixpkgs.callPackages inputs.iohkNix.utils.ciJobsAggregates
           { ciJobs = flake.hydraJobs; };
