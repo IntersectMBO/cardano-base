addee80d66e71853b48d6fa558b81e1214f0061b
lehins <lehins@users.noreply.github.com>
2024-10-14 20:08:03 +0000

    Remove all existing files

--- cardano-slotting/test/Main.hs
+++
@@ -1,8 +0,0 @@
-import Test.Tasty
-import Test.Cardano.Slotting.EpochInfo (epochInfoTests)
-
-main :: IO ()
-main = defaultMain tests
-
-tests :: TestTree
-tests = testGroup "EpochInfo" [epochInfoTests]
