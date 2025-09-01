e4d5be2469b94c47e02d2c7a9d3cb043628fa787
Joosep Jääger <joosep.jaager@iohk.io>
2025-09-01 15:47:40 +0300

    Added Eq1, Ord1, Read1 and Show1 instances for StrictMaybe

--- cardano-strict-containers/src/Data/Maybe/Strict.hs
+++ cardano-strict-containers/src/Data/Maybe/Strict.hs
@@ -29,8 +29,22 @@ import Control.Applicative (Alternative (..))
 import Control.DeepSeq (NFData)
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import Data.Default.Class (Default (..))
+import Data.Functor (($>))
+import Data.Functor.Classes (
+  Eq1 (..),
+  Ord1 (..),
+  Read1 (..),
+  Show1 (..),
+  liftReadListDefault,
+  liftReadListPrecDefault,
+  readData,
+  readUnaryWith,
+  showsUnaryWith,
+ )
 import GHC.Generics (Generic)
+import GHC.Read (expectP)
 import NoThunks.Class (NoThunks (..))
+import Text.Read (Lexeme (..), parens)
 
 data StrictMaybe a
   = SNothing
@@ -39,6 +53,7 @@ data StrictMaybe a
     ( Eq
     , Ord
     , Show
+    , Read
     , Generic
     , Functor
     , Foldable
@@ -138,3 +153,26 @@ instance Alternative StrictMaybe where
   empty = SNothing
   SNothing <|> r = r
   l <|> _ = l
+
+instance Eq1 StrictMaybe where
+  liftEq f (SJust a) (SJust b) = f a b
+  liftEq _ SNothing SNothing = True
+  liftEq _ _ _ = False
+
+instance Ord1 StrictMaybe where
+  liftCompare _ SNothing SNothing = EQ
+  liftCompare _ SNothing (SJust _) = LT
+  liftCompare _ (SJust _) SNothing = GT
+  liftCompare comp (SJust x) (SJust y) = comp x y
+
+instance Show1 StrictMaybe where
+  liftShowsPrec sp _ d (SJust x) = showsUnaryWith sp "SJust" d x
+  liftShowsPrec _ _ _ SNothing = showString "SNothing"
+
+instance Read1 StrictMaybe where
+  liftReadPrec rp _ =
+    parens (expectP (Ident "SNothing") $> SNothing)
+      <|> readData (readUnaryWith rp "SJust" SJust)
+
+  liftReadListPrec = liftReadListPrecDefault
+  liftReadList = liftReadListDefault
