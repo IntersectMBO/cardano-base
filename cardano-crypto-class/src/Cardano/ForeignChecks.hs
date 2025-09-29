3e3a1c80849119274b3d6b08fbcd0dd91b113a8b
perturbing <thomas.vellekoop@iohk.io>
2025-09-29 15:20:13 +0200

    set blst version to something different

--- cardano-crypto-class/src/Cardano/ForeignChecks.hs
+++ cardano-crypto-class/src/Cardano/ForeignChecks.hs
@@ -8,7 +8,7 @@ import Cardano.ForeignChecks.TH (
  )
 
 -- libsodium == 1.0.20
-$(ensureExactVersion "libsodium" (1, 0, 20))
+-- $(ensureExactVersion "libsodium" (1, 0, 20))
 
 -- libblst == 0.3.14 OR libblst == "commit hash"
 -- see: https://github.com/input-output-hk/iohk-nix/blob/64ca6f4c0c6db283e2ec457c775bce75173fb319/overlays/crypto/libblst.nix#L44
@@ -16,9 +16,10 @@ $(ensureExactVersion "libsodium" (1, 0, 20))
 -- and: https://github.com/supranational/blst/commit/8c7db7fe8d2ce6e76dc398ebd4d475c0ec564355
 $( ensureExactVersionOrCommit
      "libblst"
-     (Just (0, 3, 14))
-     [ "8c7db7fe8d2ce6e76dc398ebd4d475c0ec564355"
-     , "03b5124029979755c752eec45f3c29674b558446"
-     , "3dd0f804b1819e5d03fb22ca2e6fac105932043a"
-     ]
+     (Just (0, 3, 13))
+     []
+    --  [ "8c7db7fe8d2ce6e76dc398ebd4d475c0ec564355"
+    --  , "03b5124029979755c752eec45f3c29674b558446"
+    --  , "3dd0f804b1819e5d03fb22ca2e6fac105932043a"
+    --  ]
  )
