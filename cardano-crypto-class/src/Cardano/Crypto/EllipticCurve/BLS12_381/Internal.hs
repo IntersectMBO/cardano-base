cb16f8f1740757cb63e2c044cdc6fdbb8f2803a2
perturbing <thomas.vellekoop@iohk.io>
2025-12-05 10:48:57 +0100

    make blst struct staticly typed

--- cardano-crypto-class/src/Cardano/Crypto/EllipticCurve/BLS12_381/Internal.hs
+++ cardano-crypto-class/src/Cardano/Crypto/EllipticCurve/BLS12_381/Internal.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
@@ -169,6 +170,8 @@ module Cardano.Crypto.EllipticCurve.BLS12_381.Internal (
   millerLoop,
 )
 where
+
+#include "blst_util.h"
 
 import Control.Monad (forM_)
 import Data.Bits (shiftL, shiftR, (.|.))
@@ -368,7 +371,7 @@ withNewPT' :: (PTPtr -> IO a) -> IO PT
 withNewPT' = fmap snd . withNewPT
 
 sizePT :: Int
-sizePT = fromIntegral c_size_blst_fp12
+sizePT = BLST_FP12_SIZE
 
 ---- Curve operations
 
@@ -434,10 +437,10 @@ instance BLS Curve1 where
   c_blst_p_is_equal = c_blst_p1_is_equal
   c_blst_p_is_inf = c_blst_p1_is_inf
 
-  sizePoint_ _ = c_size_blst_p1
+  sizePoint_ _ = BLST_P1_SIZE
   compressedSizePoint_ _ = 48
   serializedSizePoint_ _ = 96
-  sizeAffine_ _ = c_size_blst_affine1
+  sizeAffine_ _ = BLST_AFFINE1_SIZE
 
 instance BLS Curve2 where
   c_blst_on_curve = c_blst_p2_on_curve
@@ -466,10 +469,10 @@ instance BLS Curve2 where
   c_blst_p_is_equal = c_blst_p2_is_equal
   c_blst_p_is_inf = c_blst_p2_is_inf
 
-  sizePoint_ _ = c_size_blst_p2
+  sizePoint_ _ = BLST_P2_SIZE
   compressedSizePoint_ _ = 96
   serializedSizePoint_ _ = 192
-  sizeAffine_ _ = c_size_blst_affine2
+  sizeAffine_ _ = BLST_AFFINE2_SIZE
 
 instance BLS curve => Eq (Affine curve) where
   a == b = unsafePerformIO $
@@ -480,7 +483,7 @@ instance BLS curve => Eq (Affine curve) where
 ---- Safe Scalar types / marshalling
 
 sizeScalar :: Int
-sizeScalar = fromIntegral c_size_blst_scalar
+sizeScalar = BLST_SCALAR_SIZE
 
 newtype Scalar = Scalar (ForeignPtr Void)
 
@@ -533,7 +536,7 @@ cloneScalar (Scalar a) = do
   return (Scalar b)
 
 sizeFr :: Int
-sizeFr = fromIntegral c_size_blst_fr
+sizeFr = BLST_FR_SIZE
 
 newtype Fr = Fr (ForeignPtr Void)
 
@@ -616,9 +619,6 @@ newtype ScratchPtr = ScratchPtr (Ptr Void)
 
 ---- Raw Scalar / Fr functions
 
-foreign import ccall "size_blst_scalar" c_size_blst_scalar :: CSize
-foreign import ccall "size_blst_fr" c_size_blst_fr :: CSize
-
 foreign import ccall "blst_scalar_fr_check" c_blst_scalar_fr_check :: ScalarPtr -> IO Bool
 
 foreign import ccall "blst_scalar_from_fr" c_blst_scalar_from_fr :: ScalarPtr -> FrPtr -> IO ()
@@ -630,7 +630,6 @@ foreign import ccall "blst_scalar_from_bendian"
 
 ---- Raw Point1 functions
 
-foreign import ccall "size_blst_p1" c_size_blst_p1 :: CSize
 foreign import ccall "blst_p1_on_curve" c_blst_p1_on_curve :: Point1Ptr -> IO Bool
 
 foreign import ccall "blst_p1_add_or_double"
@@ -665,7 +664,6 @@ foreign import ccall "blst_p1s_mult_pippenger"
 
 ---- Raw Point2 functions
 
-foreign import ccall "size_blst_p2" c_size_blst_p2 :: CSize
 foreign import ccall "blst_p2_on_curve" c_blst_p2_on_curve :: Point2Ptr -> IO Bool
 
 foreign import ccall "blst_p2_add_or_double"
@@ -700,9 +698,6 @@ foreign import ccall "blst_p2s_mult_pippenger"
 
 ---- Affine operations
 
-foreign import ccall "size_blst_affine1" c_size_blst_affine1 :: CSize
-foreign import ccall "size_blst_affine2" c_size_blst_affine2 :: CSize
-
 foreign import ccall "blst_p1_to_affine"
   c_blst_p1_to_affine :: AffinePtr Curve1 -> PointPtr Curve1 -> IO ()
 foreign import ccall "blst_p2_to_affine"
@@ -717,7 +712,6 @@ foreign import ccall "blst_p2_affine_in_g2" c_blst_p2_affine_in_g2 :: AffinePtr 
 
 ---- PT operations
 
-foreign import ccall "size_blst_fp12" c_size_blst_fp12 :: CSize
 foreign import ccall "blst_fp12_mul" c_blst_fp12_mul :: PTPtr -> PTPtr -> PTPtr -> IO ()
 foreign import ccall "blst_fp12_is_equal" c_blst_fp12_is_equal :: PTPtr -> PTPtr -> IO Bool
 foreign import ccall "blst_fp12_finalverify" c_blst_fp12_finalverify :: PTPtr -> PTPtr -> IO Bool
