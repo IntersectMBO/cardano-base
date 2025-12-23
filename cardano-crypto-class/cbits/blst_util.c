9905bb074974b10d6cf255ce1ca4ea22bed758e0
perturbing <thomas.vellekoop@iohk.io>
2025-12-23 16:47:25 +0100

    add `cardano_` prefix to cbits

--- cardano-crypto-class/cbits/blst_util.c
+++ cardano-crypto-class/cbits/blst_util.c
@@ -7,35 +7,35 @@
  * Verify at compile time that the sizes of the blst types match the expected ABI sizes
  * as defined in blst_util.h.
  */
-static_assert(sizeof(blst_p1) == BLST_P1_SIZE, "blst_p1 ABI size mismatch");
-static_assert(sizeof(blst_p2) == BLST_P2_SIZE, "blst_p2 ABI size mismatch");
-static_assert(sizeof(blst_scalar) == BLST_SCALAR_SIZE, "blst_scalar ABI size mismatch");
-static_assert(sizeof(blst_fr) == BLST_FR_SIZE, "blst_fr ABI size mismatch");
-static_assert(sizeof(blst_fp12) == BLST_FP12_SIZE, "blst_fp12 ABI size mismatch");
-static_assert(sizeof(blst_p1_affine) == BLST_AFFINE1_SIZE, "blst_p1_affine ABI size mismatch");
-static_assert(sizeof(blst_p2_affine) == BLST_AFFINE2_SIZE, "blst_p2_affine ABI size mismatch");
+static_assert(sizeof(blst_p1) == CARDANO_BLST_P1_SIZE, "blst_p1 ABI size mismatch");
+static_assert(sizeof(blst_p2) == CARDANO_BLST_P2_SIZE, "blst_p2 ABI size mismatch");
+static_assert(sizeof(blst_scalar) == CARDANO_BLST_SCALAR_SIZE, "blst_scalar ABI size mismatch");
+static_assert(sizeof(blst_fr) == CARDANO_BLST_FR_SIZE, "blst_fr ABI size mismatch");
+static_assert(sizeof(blst_fp12) == CARDANO_BLST_FP12_SIZE, "blst_fp12 ABI size mismatch");
+static_assert(sizeof(blst_p1_affine) == CARDANO_BLST_AFFINE1_SIZE, "blst_p1_affine ABI size mismatch");
+static_assert(sizeof(blst_p2_affine) == CARDANO_BLST_AFFINE2_SIZE, "blst_p2_affine ABI size mismatch");
 
-const int blst_success() {
+const int cardano_blst_success() {
   return BLST_SUCCESS;
 }
-const int blst_error_bad_encoding() {
+const int cardano_blst_error_bad_encoding() {
   return BLST_BAD_ENCODING;
 }
-const int blst_error_point_not_on_curve() {
+const int cardano_blst_error_point_not_on_curve() {
   return BLST_POINT_NOT_ON_CURVE;
 }
-const int blst_error_point_not_in_group() {
+const int cardano_blst_error_point_not_in_group() {
   return BLST_POINT_NOT_IN_GROUP;
 }
-const int blst_error_aggr_type_mismatch() {
+const int cardano_blst_error_aggr_type_mismatch() {
   return BLST_AGGR_TYPE_MISMATCH;
 }
-const int blst_error_verify_fail() {
+const int cardano_blst_error_verify_fail() {
   return BLST_VERIFY_FAIL;
 }
-const int blst_error_pk_is_infinity() {
+const int cardano_blst_error_pk_is_infinity() {
   return BLST_PK_IS_INFINITY;
 }
-const int blst_error_bad_scalar() {
+const int cardano_blst_error_bad_scalar() {
   return BLST_BAD_SCALAR;
 }
