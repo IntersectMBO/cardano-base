6e9e585f52f23bd5f896087e60b9e21521bc0d95
Neil Mayhew <neil.mayhew@iohk.io>
2026-01-16 16:57:53 -0700

    Move `Arbitrary` instances for `cardano-crypto-class` types into its own testlib

--- cardano-crypto-class/testlib/Test/Crypto/Instances.hs
+++ cardano-crypto-class/testlib/Test/Crypto/Instances.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Test.Crypto.Instances (
@@ -14,16 +12,21 @@ import Cardano.Crypto.DSIGN.Class hiding (Signable)
 import Cardano.Crypto.Libsodium
 import Cardano.Crypto.Libsodium.MLockedSeed
 import Cardano.Crypto.PinnedSizedBytes
+import Cardano.Crypto.Util
+import Cardano.Crypto.VRF.Class
 import Control.Monad.Class.MonadST
 import Control.Monad.Class.MonadThrow
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Short as SBS
 import Data.Maybe (mapMaybe)
+import Data.MemPack.Buffer (byteArrayFromShortByteString)
 import Data.Proxy (Proxy (Proxy))
 import GHC.Exts (fromList, fromListN, toList)
 import GHC.Stack (HasCallStack)
 import GHC.TypeLits (KnownNat, natVal)
 import System.Random.Stateful (uniformByteString)
-import Test.QuickCheck (Arbitrary (..), Gen)
+import Test.Crypto.Util (Message, arbitrarySeedOfSize)
+import Test.QuickCheck (Arbitrary (..), Gen, vectorOf)
 import qualified Test.QuickCheck.Gen as Gen
 
 -- We cannot allow this instance, because it doesn't guarantee timely
@@ -93,3 +96,40 @@ instance DSIGNAlgorithm v => Arbitrary (SigDSIGN v) where
 
 instance DSIGNAlgorithm v => Arbitrary (SignedDSIGN v a) where
   arbitrary = SignedDSIGN <$> arbitrary
+
+instance VRFAlgorithm v => Arbitrary (OutputVRF v) where
+  arbitrary = do
+    sbs <- SBS.pack <$> vectorOf (fromIntegral (sizeOutputVRF (Proxy :: Proxy v))) arbitrary
+    return $ OutputVRF $ byteArrayFromShortByteString sbs
+
+instance VRFAlgorithm v => Arbitrary (VerKeyVRF v) where
+  arbitrary = deriveVerKeyVRF <$> arbitrary
+  shrink = const []
+
+instance VRFAlgorithm v => Arbitrary (SignKeyVRF v) where
+  arbitrary = genKeyVRF <$> arbitrarySeedOfSize seedSize
+    where
+      seedSize = seedSizeVRF (Proxy :: Proxy v)
+  shrink = const []
+
+instance
+  ( VRFAlgorithm v
+  , ContextVRF v ~ ()
+  , Signable v ~ SignableRepresentation
+  ) =>
+  Arbitrary (CertVRF v)
+  where
+  arbitrary = do
+    a <- arbitrary :: Gen Message
+    sk <- arbitrary
+    return $ snd $ evalVRF () a sk
+  shrink = const []
+
+instance
+  (ContextVRF v ~ (), Signable v ~ SignableRepresentation, VRFAlgorithm v) =>
+  Arbitrary (CertifiedVRF v a)
+  where
+  arbitrary = CertifiedVRF <$> arbitrary <*> genCertVRF
+    where
+      genCertVRF :: Gen (CertVRF v)
+      genCertVRF = arbitrary
