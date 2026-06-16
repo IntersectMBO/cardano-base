75ea6ce53f13cb81e297af71a9292b574cd0aa56
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-06-16 10:31:11 +0200

    Not include slot/eb hash in leios certificate

--- cardano-crypto-leios/src/Cardano/Crypto/Leios.hs
+++ cardano-crypto-leios/src/Cardano/Crypto/Leios.hs
@@ -2,29 +2,15 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Cardano.Crypto.Leios (
-  -- * Endorser block hashes
-  EbHash (..),
-  encodeEbHash,
-  decodeEbHash,
-  prettyEbHash,
+-- | Cryptographic data types and operations used in Leios per CIP-164. Leios
+-- uses BLS12-381 MinSig as its signature scheme and definees a 'LeiosCert' that
+-- can be included into blocks.
+module Cardano.Crypto.Leios where
 
-  -- * Leios certificates
-  LeiosCert (..),
-  encodeLeiosCert,
-  decodeLeiosCert,
-
-  -- * BLS signing primitives
-  LeiosDSIGN,
-  LeiosSigningKey,
-  LeiosVerificationKey,
-  LeiosSignature,
-) where
-
-import Cardano.Binary (FromCBOR (fromCBOR), ToCBOR (toCBOR), enforceSize)
+import Cardano.Binary (enforceSize)
 import Cardano.Crypto.DSIGN (
   SigDSIGN,
   SignKeyDSIGN,
@@ -33,39 +19,15 @@ import Cardano.Crypto.DSIGN (
   encodeSigDSIGN,
  )
 import Cardano.Crypto.DSIGN.BLS12381 (BLS12381MinSigDSIGN)
-import Cardano.Slotting.Slot (SlotNo)
 import Codec.CBOR.Decoding (Decoder, decodeBytes)
 import Codec.CBOR.Encoding (Encoding, encodeBytes, encodeListLen)
-import Codec.Serialise (Serialise)
 import Control.DeepSeq (NFData)
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Base16 as Base16
-import qualified Data.ByteString.Char8 as BS8
 import GHC.Generics (Generic)
 import NoThunks.Class (NoThunks)
 
--- | Hash of the RB header that announced the endorser block.
---
--- Per CIP-0164 this is a @hash32@ (Blake2b-256, 32 bytes); the underlying
--- bytes are stored unwrapped to stay representation-compatible with the
--- previous definition in @ouroboros-consensus@.
-newtype EbHash = MkEbHash {ebHashBytes :: ByteString}
-  deriving stock (Eq, Ord, Generic)
-  deriving newtype (NFData, NoThunks, Serialise, ToCBOR, FromCBOR)
+-- * Cryptographic primitives
 
-instance Show EbHash where
-  show = prettyEbHash
-
-encodeEbHash :: EbHash -> Encoding
-encodeEbHash (MkEbHash bytes) = encodeBytes bytes
-
-decodeEbHash :: Decoder s EbHash
-decodeEbHash = MkEbHash <$> decodeBytes
-
-prettyEbHash :: EbHash -> String
-prettyEbHash (MkEbHash bytes) = BS8.unpack (Base16.encode bytes)
-
--- | Leios uses BLS12-381 MinSig as its signature scheme; see CIP-0164.
 type LeiosDSIGN = BLS12381MinSigDSIGN
 
 type LeiosSigningKey = SignKeyDSIGN LeiosDSIGN
@@ -74,47 +36,40 @@ type LeiosVerificationKey = VerKeyDSIGN LeiosDSIGN
 
 type LeiosSignature = SigDSIGN LeiosDSIGN
 
--- | A Leios certificate over an endorser block, as specified in CIP-0164:
+-- * Leios certificates
+
+-- | A Leios certificate over an endorser block, as specified in CIP-164:
 --
 -- @
 -- leios_certificate =
---   [ slot_no
---   , endorser_block_hash
---   , signers               : bytes          ; bitfield over the epoch's committee, MSB-first
+--   [ signers               : bytes ; bitfield over the epoch's committee, MSB-first
 --   , aggregated_signature  : leios_bls_signature
 --   ]
 -- @
 --
--- The committee is derived deterministically from the active stake
--- distribution for the epoch of the announcing RB, so individual voter
--- identities and eligibility proofs are not carried in the certificate;
--- 'signers' is a @⌈N\/8⌉@-byte bitfield over the committee where bit @i@
--- is set iff voter index @i@ signed.
+-- The committee is derived deterministically from the active stake distribution
+-- for the epoch of the announcing RB, so individual voter identities and
+-- eligibility proofs are not carried in the certificate; 'signers' is a
+-- @⌈N\/8⌉@-byte bitfield over the committee where bit @i@ is set iff voter
+-- index @i@ signed.
 data LeiosCert = LeiosCert
-  { slotNo :: !SlotNo
-  , endorserBlockHash :: !EbHash
-  , signers :: !ByteString
+  { signers :: !ByteString
   , aggregatedSignature :: !LeiosSignature
   }
   deriving stock (Show, Eq, Generic)
   deriving anyclass (NFData, NoThunks)
 
--- | Plain CBOR encoder for 'LeiosCert', matching the CDDL above. Use this
--- (rather than a 'ToCBOR'/'EncCBOR' class instance) at call sites so we
--- don't need orphan CBOR instances for the underlying BLS 'SigDSIGN'.
+-- | Plain CBOR encoder for 'LeiosCert', matching the CDDL in 'LeiosCert'.
 encodeLeiosCert :: LeiosCert -> Encoding
 encodeLeiosCert cert =
   encodeListLen 4
-    <> toCBOR (slotNo cert)
-    <> encodeEbHash (endorserBlockHash cert)
-    <> encodeBytes (signers cert)
-    <> encodeSigDSIGN (aggregatedSignature cert)
+    <> encodeBytes cert.signers
+    <> encodeSigDSIGN cert.aggregatedSignature
 
+-- | Plain CBOR decoder for 'LeiosCert', matching the CDDL in 'LeiosCert'.
 decodeLeiosCert :: Decoder s LeiosCert
 decodeLeiosCert = do
   enforceSize "LeiosCert" 4
   LeiosCert
-    <$> fromCBOR
-    <*> decodeEbHash
-    <*> decodeBytes
+    <$> decodeBytes
     <*> decodeSigDSIGN
